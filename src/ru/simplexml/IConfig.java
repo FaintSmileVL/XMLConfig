5cab3ba97544351a29f027daea3dba37ad39ec61
FaintSmileVL <faintsmilevl@gmail.com>
2025-04-24 19:29:14 +0300

    * update XMLConfig * change hierarchy * make some methods protected

--- src/ru/simplexml/IConfig.java
+++ src/ru/simplexml/IConfig.java
@@ -1,25 +1,11 @@
 package ru.simplexml;
 
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author : faint
  * @date : 02.07.2023
  * @time : 14:47
  */
 public interface IConfig extends IXMLReader {
-    void load();
-
-    void putValue(String key, Object value);
-
     Object getValue(String key);
 
     String getValueStr(String key, String dflt);
@@ -31,46 +17,4 @@ public interface IConfig extends IXMLReader {
     long getValueLong(String key, Long dflt);
 
     boolean getValueBool(String key, Boolean dflt);
-
-    void compute(String key, Object value);
-
-    default Document getDocument(String path) throws ParserConfigurationException, IOException, SAXException {
-        var file = new File(path);
-        var factory1 = DocumentBuilderFactory.newInstance();
-        factory1.setValidating(false);
-        factory1.setIgnoringComments(true);
-        return factory1.newDocumentBuilder().parse(file);
-    }
-
-    default Document getDocument(File file) throws ParserConfigurationException, IOException, SAXException {
-        var factory1 = DocumentBuilderFactory.newInstance();
-        factory1.setValidating(false);
-        factory1.setIgnoringComments(true);
-        return factory1.newDocumentBuilder().parse(file);
-    }
-
-    /**
-     * Парсит все XML-файлы в указанной папке и возвращает список документов.
-     *
-     * @param folderPath Путь к папке с XML-файлами.
-     * @return Список успешно обработанных XML-документов.
-     */
-    default List<Document> getDocuments(String folderPath) throws ParserConfigurationException, IOException, SAXException {
-        var documents = new ArrayList<Document>();
-        var folder = new File(folderPath);
-        if (!folder.isDirectory()) {
-            return documents;
-        }
-        var files = folder.listFiles();
-        if (files == null || files.length == 0) {
-            return documents;
-        }
-        for (File file : files) {
-            if (!file.isFile() && file.getName().toLowerCase().endsWith(".xml")) {
-                continue;
-            }
-            documents.add(getDocument(file));
-        }
-        return documents;
-    }
 }
--- src/ru/simplexml/XMLConfig.java
+++ src/ru/simplexml/XMLConfig.java
@@ -4,8 +4,16 @@ import org.apache.commons.lang.math.NumberUtils;
 import org.slf4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+import ru.StringUtils;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author : faint
@@ -14,8 +22,8 @@ import java.util.HashMap;
  */
 public abstract class XMLConfig implements IConfig {
     public HashMap<String, Object> settings = new HashMap<>();
-    @Override
-    public void putValue(String key, Object value) {
+
+    protected void putValue(String key, Object value) {
         settings.putIfAbsent(key, value);
     }
 
@@ -49,12 +57,11 @@ public abstract class XMLConfig implements IConfig {
         return (Boolean)settings.getOrDefault(key, dflt);
     }
 
-    @Override
-    public void compute(String key, Object value) {
+    protected void compute(String key, Object value) {
         settings.computeIfPresent(key, (k, v) -> value);
     }
 
-    public void parseConfig(Node node, Logger log) {
+    protected void parseConfig(Node node, Logger log) {
         if (get(node, "name") == null || get(node, "name").isEmpty()) {
             log.warn("Error parsing config file conf = {}", node.getNodeName());
             return;
@@ -75,7 +82,7 @@ public abstract class XMLConfig implements IConfig {
      * @param doc
      * @return
      */
-    public Node getFirstChildOfList(Document doc) {
+    protected Node getFirstChildOfList(Document doc) {
         return getFirstChildOf(doc, "list");
     }
 
@@ -86,12 +93,191 @@ public abstract class XMLConfig implements IConfig {
      * @param nodeName
      * @return
      */
-    public Node getFirstChildOf(Node head, String nodeName) {
+    protected Node getFirstChildOf(Node head, String nodeName) {
         for (var node = head.getFirstChild(); node != null; node = node.getNextSibling()) {
             if (isNodeName(node, nodeName)) {
                 return node.getFirstChild();
             }
         }
         return head;
+    }
+
+    protected String get(Node n, String item) {
+        try {
+            final Node d = n.getAttributes().getNamedItem(item);
+            if (d == null) {
+                return StringUtils.EMPTY;
+            }
+            final String val = d.getNodeValue();
+            if (val == null) {
+                return StringUtils.EMPTY;
+            }
+            return val;
+        } catch (Exception e) {
+            return StringUtils.EMPTY;
+        }
+    }
+
+    protected boolean get(Node n, String item, boolean dflt) {
+        try {
+            final Node d = n.getAttributes().getNamedItem(item);
+            if (d == null) {
+                return dflt;
+            }
+            final String val = d.getNodeValue();
+            if (val == null) {
+                return dflt;
+            }
+            return Boolean.parseBoolean(val);
+        } catch (Exception e) {
+            return dflt;
+        }
+    }
+
+    protected byte get(Node n, String item, byte dflt) {
+        try {
+            final Node d = n.getAttributes().getNamedItem(item);
+            if (d == null) {
+                return dflt;
+            }
+            final String val = d.getNodeValue();
+            if (val == null) {
+                return dflt;
+            }
+            return Byte.parseByte(val);
+        } catch (Exception e) {
+            return dflt;
+        }
+    }
+
+    protected int get(Node n, String item, int dflt) {
+        try {
+            final Node d = n.getAttributes().getNamedItem(item);
+            if (d == null) {
+                return dflt;
+            }
+            final String val = d.getNodeValue();
+            if (val == null) {
+                return dflt;
+            }
+            return Integer.parseInt(val);
+        } catch (Exception e) {
+            return dflt;
+        }
+    }
+
+    protected long get(Node n, String item, long dflt) {
+        try {
+            final Node d = n.getAttributes().getNamedItem(item);
+            if (d == null) {
+                return dflt;
+            }
+            final String val = d.getNodeValue();
+            if (val == null) {
+                return dflt;
+            }
+            return Long.parseLong(val);
+        } catch (Exception e) {
+            return dflt;
+        }
+    }
+
+    protected double get(Node n, String item, double dflt) {
+        try {
+            final Node d = n.getAttributes().getNamedItem(item);
+            if (d == null) {
+                return dflt;
+            }
+            final String val = d.getNodeValue();
+            if (val == null) {
+                return dflt;
+            }
+            return Double.parseDouble(val);
+        } catch (Exception e) {
+            return dflt;
+        }
+    }
+
+    protected int[] get(Node n, String item, int[] dflt) {
+        try {
+            final Node d = n.getAttributes().getNamedItem(item);
+            if (d == null) {
+                return dflt;
+            }
+            final String val = d.getNodeValue();
+            if (val == null) {
+                return dflt;
+            }
+            String strs[] = get(n, item).split(";");
+            int values[] = new int[strs.length];
+            for (int i = 0; i < strs.length; i++) {
+                values[i] = Integer.parseInt(strs[i]);
+            }
+            return values;
+        } catch (Exception e) {
+            return dflt;
+        }
+    }
+
+    protected <E extends Enum<E>> E get(Node n, String item, Class<E> enumClass) {
+        final Node d = n.getAttributes().getNamedItem(item);
+        if (d == null) {
+            return null;
+        }
+        final Object val = d.getNodeValue();
+        if (val == null) {
+            return null;
+        }
+        if (val != null && enumClass.isInstance(val)) {
+            return (E) val;
+        }
+        if (val instanceof String) {
+            return Enum.valueOf(enumClass, (String) val);
+        }
+        throw new IllegalArgumentException("Enum value of type " + enumClass.getName() + " required, but found: " + val + "!");
+    }
+
+    protected boolean isNodeName(Node node, String name) {
+        return node != null && node.getNodeName().equals(name);
+    }
+
+    protected Document getDocument(String path) throws ParserConfigurationException, IOException, SAXException {
+        var file = new File(path);
+        var factory1 = DocumentBuilderFactory.newInstance();
+        factory1.setValidating(false);
+        factory1.setIgnoringComments(true);
+        return factory1.newDocumentBuilder().parse(file);
+    }
+
+    protected Document getDocument(File file) throws ParserConfigurationException, IOException, SAXException {
+        var factory1 = DocumentBuilderFactory.newInstance();
+        factory1.setValidating(false);
+        factory1.setIgnoringComments(true);
+        return factory1.newDocumentBuilder().parse(file);
+    }
+
+    /**
+     * Парсит все XML-файлы в указанной папке и возвращает список документов.
+     *
+     * @param folderPath Путь к папке с XML-файлами.
+     * @return Список успешно обработанных XML-документов.
+     */
+    protected List<Document> getDocuments(String folderPath) throws ParserConfigurationException, IOException, SAXException {
+        var documents = new ArrayList<Document>();
+        var folder = new File(folderPath);
+        if (!folder.isDirectory()) {
+            return documents;
+        }
+        var files = folder.listFiles();
+        if (files == null || files.length == 0) {
+            return documents;
+        }
+        for (File file : files) {
+            if (!file.isFile() && file.getName().toLowerCase().endsWith(".xml")) {
+                continue;
+            }
+            documents.add(getDocument(file));
+        }
+        return documents;
     }
 }
